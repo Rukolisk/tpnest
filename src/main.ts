9210e449e8bc245a0b32cfb969707d3a166e7d39
Rukolisk <bastien.fournier50943@gmail.com>
2026-04-29 16:16:55 +0200

    Enable global validation pipe and add class-transformer dependency

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // supprime les champs non déclarés dans le DTO
+      forbidNonWhitelisted: true, // retourne 400 si champ inconnu envoyé
+      transform: true, // transforme automatiquement les types
+    }),
+  );
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
